0860dca6ffeed8e612913ff98abfbfd0c9edb289
EC2 Default User <ec2-user@ip-172-31-35-88.ap-northeast-1.compute.internal>
2019-06-21 01:11:41 +0000

    Adjust the layout of some blades

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,7 @@
     <title>フルーツ☆パラダイス</title>
     <!--bootstrapのCDN-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/toppage.css" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -16,26 +17,27 @@
 <body>
     <!--ヘッダー開始-->
     <header>
-      <nav class="navbar navbar-expand-md navbar-light">
-    <div class="container-fluid px-3 pt-2">
-        <a class="navbar-brand" href="{{ route('index') }}">フルーツ☆パラダイス</a>
-        <button type="button" class="navbar-toggler collapsed" data-toggle="collapse" data-target="#Navbar" aria-controls="Navbar" aria-expanded="false" aria-label="ナビゲーションの切替"><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse justify-content-end" id="Navbar">
-        <ul class="navbar-nav d-flex flex-column flex-md-row">
-            <li class="nav-item header-list"><a href="{{ route('index') }}" class="nav-link text-center">Home</a></li>
-            <li class="nav-item header-list"><a href="{{ route('about') }}" class="nav-link text-center">当サイトについて</a></li>
-            <li class="nav-item header-list"><a href="{{ route('product.list') }}" class="nav-link text-center">商品一覧</a></li>
-            <li class="nav-item header-list"><a href="{{ route('admin.signin') }}" class="nav-link text-center">管理者ログイン</a></li>
+    <nav class="navbar navbar-expand-sm navbar-light bg-warning justify-content-center header-nav">
+      <div class="container-fluid header">
+        <div class="row header-row">
+          <div class="col-12 hdlogo"><a class="navbar-brand" href="{{ route('index') }}">フルーツ☆パラダイス</a></div>
+          <div class="col-12">
+            <ul class="navbar-nav">
+            <li class="nav-item header-list"><a href="{{ route('index') }}" class="nav-link">Home</a></li>
+            <li class="nav-item header-list"><a href="{{ route('about') }}" class="nav-link">当サイトについて</a></li>
+            <li class="nav-item header-list"><a href="{{ route('product.list') }}" class="nav-link">商品一覧</a></li>
+            <li class="nav-item header-list"><a href="{{ route('admin.signin') }}" class="nav-link">管理者ログイン</a></li>
             @if( Auth::check() )
-            <li class="nav-item header-list"><a href="{{ route('users.logout') }}" class="nav-link text-center">ログアウト</a></li>
+            <li class="nav-item header-list"><a href="{{ route('users.logout') }}" class="nav-link">ログアウト</a></li>
             <li class="nav-item header-list"><a href="#" class="nav-link text-center">カート</a></li>
             @else
-            <li class="nav-item header-list"><a href="{{ route('users.signin') }}" class="nav-link text-center">ログイン</a></li>
-            <li class="nav-item header-list"><a href="{{ route('users.signup') }}" class="nav-link text-center">会員登録</a></li>
+            <li class="nav-item header-list"><a href="{{ route('users.signin') }}" class="nav-link">ログイン</a></li>
+            <li class="nav-item header-list"><a href="{{ route('users.signup') }}" class="nav-link">会員登録</a></li>
             @endif
-        </ul>
+          </ul>
+          </div>
         </div>
-    </div>
+      </div>
     </nav>
     </header>
     <!--ヘッダー終了-->
@@ -54,10 +56,10 @@
       <div class="col-12 col-sm-6">
           <nav class="navbar navbar-expand-md navbar-light justify-content-center">
             <ul class="nav navbar-nav my-3 list-inline">
-                <li class="nav-item list-inline-item"><a href="#" class="nav-link"><i class="fab fa-facebook social-icon-color"><span class="d-none d-md-inline">FACEBOOK</span></i></a></li>
-                <li class="nav-item list-inline-item"><a href="#" class="nav-link"><i class="fab fa-twitter-square social-icon-color"><span class="d-none d-md-inline">TWITTER</span></i></a></li>
-                <li class="nav-item list-inline-item"><a href="#" class="nav-link"><i class="fab fa-instagram social-icon-color"><span class="d-none d-md-inline">INSTAGRAM</span></i></a></li>
-                <li class="nav-item list-inline-item"><a href="#" class="nav-link"><i class="fab fa-linkedin social-icon-color"><span class="d-none d-md-inline">LINKEDIN</span></i></a></li>
+                <li class="nav-item"><a href="#" class="nav-link"><i class="fab fa-facebook social-icon-color">FACEBOOK</i></a></li>
+                <li class="nav-item"><a href="#" class="nav-link"><i class="fab fa-twitter-square social-icon-color">TWITTER</i></a></li>
+                <li class="nav-item"><a href="#" class="nav-link"><i class="fab fa-instagram social-icon-color">INSTAGRAM</i></a></li>
+                <li class="nav-item"><a href="#" class="nav-link"><i class="fab fa-linkedin social-icon-color">LINKEDIN</i></a></li>
             </ul>
           </nav>
       </div>
--- resources/views/product_list.blade.php
+++ resources/views/product_list.blade.php
@@ -4,17 +4,22 @@
 <h1>商品一覧</h1>
 <div class="container-fluid">
     <div class="row">
-        @foreach($products as $product)
-        <div class="col-12 col-sm-6 col-lg-4">
-            <img src="{{ $product->product_image }}" class="img-fluid" alt="商品イメージ">
-            <h2>{{ $product->product_name }}</h2>
-            <p>価格 {{ $product->price }} 円</p>
-            <a href="{{ route('product.show') }}">商品詳細</a>
-            
-        </div>
-        @endforeach
+    @foreach($products as $product)
+  <div class="col-12 col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="{{ $product->product_image }}" class="img-fluid" alt="商品イメージ">
+      <div class="caption">
+        <h2>{{ $product->product_name }}</h2>
+        <p>価格 {{ $product->price }} 円</p>
+        <p><a href="{{ route('product.show', $product->id) }}" class="btn btn-primary" role="button">商品詳細</a></p>
+      </div>
     </div>
+  </div>
+  @endforeach
 </div>
+</div>
+
+
   
 @endsection
   
